09608506a20a7ed52ad9a2b33837790d09d25b4f
James Thigpen <james.r.thigpen@gmail.com>
2017-02-27 16:15:04 -0800

    [OPS-1076] Debug dd metric push.

--- monitor.py
+++ monitor.py
@@ -1,12 +1,14 @@
 import alooma
 import datadog
+import datetime
 import os
 import time
 
 ALOOMA_USERNAME = os.environ.get('ALOOMA_USERNAME')
 ALOOMA_PASSWORD = os.environ.get('ALOOMA_PASSWORD')
 DATADOG_API_KEY = os.environ.get('DATADOG_API_KEY')
-TIME_SLEEP = 300
+MINUTES_SLEEP = 3
+SECONDS_SLEEP = MINUTES_SLEEP * 60
 
 
 api = alooma.Alooma(
@@ -17,6 +19,12 @@ api = alooma.Alooma(
 
 datadog.initialize(api_key=DATADOG_API_KEY)
 
+
+def posix_timestamp():
+    d = datetime.datetime.now()
+    return str(int(time.mktime(d.timetuple())))
+
+
 def send_metric(data):
     for d in data:
         metric_name = "alooma.{}".format(d['target'].lower())
@@ -26,7 +34,7 @@ def send_metric(data):
             x.reverse()
 
         for v in values:
-            # v[0] = v[0] / 10.0
+            v[0] = str(v[0])
             v[1] = float(0 if v[1] is None else v[1])
 
         values = [tuple(x) for x in values]
@@ -52,12 +60,20 @@ metrics.remove('MEMORY_LEFT')
 
 while True:
     for m in metrics:
-        data = api.get_metrics_by_names(m, 1)
+        data = api.get_metrics_by_names(m, MINUTES_SLEEP)
         print "Sending {}".format(m)
         result = send_metric(data)
 
         for r in result:
             import pprint; pprint.pprint(r)
 
-    print "Sleeping for {}".format(TIME_SLEEP)
-    time.sleep(TIME_SLEEP)
+    inputs = api.get_inputs()
+    num_inputs = len(inputs)
+    result = datadog.api.Metric.send(
+        metric='alooma.num_inputs',
+        points=[(posix_timestamp(), num_inputs)],
+        type='gauge',
+    )
+
+    print "Sleeping for {}m".format(MINUTES_SLEEP)
+    time.sleep(SECONDS_SLEEP)
